dd3165d32251cef05389657e41b224a1ab9f2e0a
Jeanne Locker <jeannelocker@earthlink.net>
2014-12-22 08:38:01 -0500

    basechar stuff

---
+++ basechar.java
@@ -0,0 +1,12 @@
+public class basechar{
+
+    private String name = "";
+    
+    public void inputName(String name){
+	this.name = name;
+    }
+
+    public String getName(){
+	return this.name;
+    }
+}
